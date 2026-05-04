2ad03ae70d59c5a963aa17ae6d8261b87bd01d2f
shozi-exe <shozabdocx@gmail.com>
2026-05-04 21:50:33 +0500

    changes

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-console.log("Hello World!");
 function getInput(){
-let input = document.querySelector(".search input").value;
-console.log(input);
+let input = document.getElementById("type").value;
+
+let email = "shozabdocx@gmail.com";
+let subject = "Hello Shozab";
+let body = "Hi , I visited your website . I am " + input;
+
+let gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`;
+window.open(gmailUrl, "_blank");
+
+document.getElementById("type").value = "";
+}
+
+function openWP(){
+    let message = "Hi Shozab , I visited your vebsite .";
+    let url = `https://wa.me/923001234567?text=${encodeURIComponent(message)}`;
+    window.open(url, "_blank");
 }
